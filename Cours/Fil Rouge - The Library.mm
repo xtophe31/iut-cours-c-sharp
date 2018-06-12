ab972602e96a88b13c03404732c91440fe6956ec
xtophe31 <xtophe31@users.noreply.github.com>
2018-06-12 11:48:13 +0200

    Add files via upload

---
+++ Cours/Fil Rouge - The Library.mm
@@ -0,0 +1,97 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Fil Rouge&#xa;The Library" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1417366762633"><hook NAME="MapStyle" zoom="1.464">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9"/>
+<node TEXT="Tableau de bord" POSITION="right" ID="ID_384608382" CREATED="1417366689216" MODIFIED="1417366696411">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Clients" POSITION="right" ID="ID_1403097636" CREATED="1417366328649" MODIFIED="1417366331295">
+<edge COLOR="#0000ff"/>
+<node TEXT="Commandes" ID="ID_1341396945" CREATED="1417366366977" MODIFIED="1417366673686"/>
+<node TEXT="Factures" ID="ID_1094082478" CREATED="1417366370865" MODIFIED="1417366671521"/>
+</node>
+<node TEXT="Prestations" POSITION="right" ID="ID_23850746" CREATED="1417366361170" MODIFIED="1417366364144">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Reliure" ID="ID_549431659" CREATED="1417366475760" MODIFIED="1417366482175"/>
+<node TEXT="R&#xe9;paration" ID="ID_1738954671" CREATED="1417366483761" MODIFIED="1417366487341"/>
+<node TEXT="Impression" ID="ID_1684845199" CREATED="1417366496448" MODIFIED="1417366502415"/>
+<node TEXT="Num&#xe9;risation" ID="ID_694039760" CREATED="1417366509888" MODIFIED="1417366513006"/>
+</node>
+<node TEXT="Stock" POSITION="left" ID="ID_505056163" CREATED="1417366377394" MODIFIED="1417366382671">
+<edge COLOR="#7c0000"/>
+<node TEXT="Livres" ID="ID_1068945785" CREATED="1417366333585" MODIFIED="1417366541965">
+<node TEXT="Editions" ID="ID_215897764" CREATED="1417366432033" MODIFIED="1417366523346"/>
+<node TEXT="Auteurs" ID="ID_1335599309" CREATED="1417366322845" MODIFIED="1417366541964"/>
+<node TEXT="Editeur" ID="ID_223050088" CREATED="1417366544672" MODIFIED="1417366560909"/>
+<node TEXT="Physique" ID="ID_797220778" CREATED="1417366570336" MODIFIED="1417366590669">
+<node TEXT="Dimensions" ID="ID_622481981" CREATED="1417366592960" MODIFIED="1417366597405"/>
+<node TEXT="Format" ID="ID_143158392" CREATED="1417366607265" MODIFIED="1417366610349">
+<node TEXT="Broch&#xe9;" ID="ID_46397349" CREATED="1417366613248" MODIFIED="1417366617869"/>
+<node TEXT="Poche" ID="ID_791067027" CREATED="1417366618655" MODIFIED="1417366621230"/>
+<node TEXT="Reli&#xe9;" ID="ID_672552355" CREATED="1417366622240" MODIFIED="1417366624989"/>
+</node>
+<node TEXT="Poids" ID="ID_1464671044" CREATED="1417366598256" MODIFIED="1417366599981"/>
+<node TEXT="Nombre page" ID="ID_1864879401" CREATED="1417366601151" MODIFIED="1417366604142"/>
+</node>
+<node TEXT="Tags" ID="ID_320950931" CREATED="1417366562032" MODIFIED="1417366566045"/>
+</node>
+<node TEXT="Papeterie" ID="ID_730943355" CREATED="1417366807774" MODIFIED="1417417702090"/>
+</node>
+<node TEXT="Fournisseurs" POSITION="left" ID="ID_1937573026" CREATED="1417366866462" MODIFIED="1417366870924">
+<edge COLOR="#007c00"/>
+<node TEXT="Correspondant" ID="ID_114103652" CREATED="1417366887726" MODIFIED="1417366894203"/>
+<node TEXT="Commandes" ID="ID_1009718961" CREATED="1417366895405" MODIFIED="1417366932139">
+<node TEXT="Quantit&#xe9; Min" ID="ID_1922801298" CREATED="1417368092902" MODIFIED="1417368111891"/>
+<node TEXT="Unit&#xe9; de commande" ID="ID_1611041287" CREATED="1417368112437" MODIFIED="1417368116403"/>
+</node>
+</node>
+</node>
+</map>
